f1b4ddbc78c5d32a057699c56a962bdb883bc930
Mark Perun <maperun@hotmail.com>
2017-09-17 11:29:07 -0400

    added functions to gallery and contact pages

---
+++ Contact/main.js
@@ -0,0 +1,6 @@
+function submitForm() {
+    alert('Received,thank you!');
+}
+
+
+
--- Image_Gallery/main.js
+++ Image_Gallery/main.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
 
-$('img').click(function() {
-    $(this).css('width','40%');
+	$('img').click(function() {
+		if (!$(this).hasClass('enlarged')) {
+			$(this).addClass('enlarged');
+	    	$(this).css('width', '40%');
+		} else {
+			$(this).removeClass('enlarged');
+			$(this).css('width','20%');
+	    }
    	});
 });
+
+
+
